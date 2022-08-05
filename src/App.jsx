44a84e872bd8ea497fb1035999c2c3058258b885
VilmaPetruskeviciene <105633730+VilmaPetruskeviciene@users.noreply.github.com>
2022-08-05 10:15:35 +0300

    jokes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 
 function App() {
 
-    
+    const [jokes, setJokes] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://v2.jokeapi.dev/joke/Programming?amount=10')
+        .then(res => setJokes(res.data.jokes));
+    }, []); 
   
     return (
       <div className="App">
         <header className="App-header">
-            <h1>API</h1>
+            <h1>JOKE</h1>
+            <p>
+                {
+                    jokes.map(j => <p key={j.id}>{j.joke}</p>)
+                }
+                {
+                    jokes.map(u => <p key={u.id}>{u.setup}{u.delivery}</p>)
+                }
+            </p>
             
+                
 
         </header>
       </div>
